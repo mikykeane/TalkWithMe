1b480eda4783ef58399a5f966745c7fd0cd440e0
mikykeane <miguekeca@gmail.com>
2017-02-03 22:44:00 +0100

    Added classes

---
+++ app/src/main/java/ugr/npi/talkwithme/tera/BotHandler.java
@@ -0,0 +1,8 @@
+package ugr.npi.talkwithme.tera;
+
+/**
+ * Created by migue on 03/02/2017.
+ */
+
+public class BotHandler {
+}
---
+++ app/src/main/java/ugr/npi/talkwithme/voiceinterface/VoiceActivity.java
@@ -0,0 +1,23 @@
+package ugr.npi.talkwithme.voiceinterface;
+
+import android.app.Activity;
+import android.speech.RecognitionListener;
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public abstract class VoiceActivity extends AppCompatActivity implements RecognitionListener, TextToSpeech.OnInitListener{
+
+    private RecognitionListener RL;
+    private TextToSpeech T2S;
+    Activity act;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voice);
+
+
+    }
+}
